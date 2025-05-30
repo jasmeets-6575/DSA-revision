a9c81217e0b70ead65d1f80ab9f93d308c2cb666
Jasmeet Singh <jasmeetsonu990@gmail.com>
2025-05-30 14:44:01 +0530

    learning arrays

---
+++ by-abdul-bari/04-arrays-adt/13-Sum-pair.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Array {
+    int A[10];  // Array with a fixed size of 10, stored on the stack
+    int size;   // Total capacity of the array
+    int length; // Current number of elements in the array
+};
+
+void Display(struct Array arr)
+{
+    printf("Elements are:\n");
+    for (int i = 0; i < arr.length; i++)
+        printf("%d ", arr.A[i]);
+    printf("\n");
+}
+
+int Pair(struct Array *arr)
+{
+    int i , same;
+    same=0;
+    for(i=0; i<arr->length -1; i++)
+    {
+        if(arr->A[i] == arr->A[i+1] && arr->A[i] != same)
+        {
+            printf("%d\n", arr->A[i]);
+            same = arr->A[i];
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    int r;
+    struct Array arr1 = {{1,2,3,4,6,7,8}, 10, 7};  
+    struct Array arr2 = {{8,9,11,11,15,16,16,16}, 10, 8};  
+    // &r = Missing(&arr2);
+    printf("Element no. %d \n", Pair(&arr2));
+    Display(arr2);
+
+
+    return 0;
+}
