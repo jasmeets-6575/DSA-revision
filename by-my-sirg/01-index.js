3f38460bbcc9e0bf9e53ad2a70489d8d5bf23ee4
Jasmeet Singh <jasmeetsonu990@gmail.com>
2025-06-26 14:53:07 +0530

    learning pattens

---
+++ by-my-sirg/01-index.js
@@ -0,0 +1,20 @@
+function print () {
+    for (let i = 0; i < 3 ; i++ ){
+        let row = ''
+        for (let j =0; j < 5; j ++){
+            row += '*';
+        }
+        console.log(row);
+    }
+}
+function print () {
+    for (let i = 0; i < 5 ; i++ ){
+        let row = ''
+        for (let j =0; j <= i ; j ++){
+            row += '*';
+        }
+        console.log(row);
+    }
+}
+
+print()
