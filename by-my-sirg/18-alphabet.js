0a82ef1c0ea9403315d9e4c5e11b74e8edcf7966
Jasmeet Singh <jasmeetsonu990@gmail.com>
2025-07-29 14:56:59 +0530

    learning pattens

---
+++ by-my-sirg/18-alphabet.js
@@ -0,0 +1,15 @@
+(function (){
+    for (let i = 1; i <=4 ; i++ ){
+        let line  = ''; 
+        let k = 0;
+        for (let j= 1; j <= 7; j++ ){
+            if (j>= 5 - i && j <= 3 + i ) {
+                line += String.fromCharCode(65 + k);
+                j < 4 ? k++ : k--
+            } else { 
+                line += ' '
+            }
+        }
+        console.log(line);
+    }
+})()
