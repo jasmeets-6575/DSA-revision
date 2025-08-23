fd18df30fa677375f145ff44d465b25a6b564df6
Jasmeet Singh <jasmeetsonu990@gmail.com>
2025-08-23 15:00:50 +0530

    learning pattens

---
+++ by-my-sirg/30-alphabet-number.js
@@ -0,0 +1,13 @@
+(function () {
+    for (let i = 1; i <= 4; i++) {
+        let line = '';
+        let k = 0
+        for (let j = 1; j <= 7; j++) {
+            if (j >= 5 - i && j <= 3 + i) {
+                line += k + i
+                j > 3 ? k++ : k--
+            } else line += ' '
+        }
+        console.log(line);
+    }
+})()
