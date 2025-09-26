3bc0854190abd44129e77282602600890b7829fc
Jasmeet Singh <jasmeetsonu990@gmail.com>
2025-09-26 16:23:55 +0530

    learning linked list

---
+++ by-abdul-bari/07-linked-list/09-deleting-node.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node * next;
+} * first = NULL;
+
+void create ( int A[], int n)
+{
+    int i;
+    struct Node *temp, *last;
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next= NULL;
+    last = first;
+
+    for (i=1; i<n; i++)
+    {
+        temp= (struct Node*)malloc(sizeof(struct Node));
+        temp->data = A[i];
+        temp->next = NULL;
+        last->next = temp;
+        last = temp;
+    }
+}
+
+int count ( struct Node *p)
+{
+    int count = 0 ;
+    while (p)
+    {
+        count ++;
+        p=p->next;
+    }
+
+    return count;
+}
+
+int delete( struct Node *p , int index)
+{
+    struct Node *q=NULL;
+    int x= -1, i;
+
+    if( index < 1 || index > count(first))
+        return -1;
+    
+    if ( index == 1)
+    {
+        q=first;
+        x=q->data;
+        first=first->next;
+        free(q);
+        return x;
+    }
+    else 
+    {
+        for(i=0;i<index -1; i++)
+        {
+            q=p;
+            p=p->next;
+        }
+        q->next=p->next;
+        x=p->data;
+        free(p);
+        return x;
+    }
+}
+
+
+void display(struct Node*p)
+{
+    while (p!= NULL)
+    {
+        printf("%d ", p->data);
+        p=p->next;
+    }
+}
+
+int main ()
+{
+    int A[] = { 3, 5, 7, 10 , 15};
+
+    create(A, 5);
+    display(first);
+
+    // index
+    delete(first , 3);
+    display(first);
+    return 0;
+}
