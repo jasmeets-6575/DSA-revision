2506df60fe95acb9bdb6390bf2f61d95b47c7620
Jasmeet Singh <jasmeetsonu990@gmail.com>
2025-09-27 16:24:29 +0530

    learning linked list

---
+++ by-abdul-bari/07-linked-list/11-duplicate.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node * next;
+} * first = NULL;
+
+void create ( int A[], int n)
+{
+    int i;
+    struct Node *temp, *last;
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next= NULL;
+    last = first;
+
+    for (i=1; i<n; i++)
+    {
+        temp= (struct Node*)malloc(sizeof(struct Node));
+        temp->data = A[i];
+        temp->next = NULL;
+        last->next = temp;
+        last = temp;
+    }
+}
+
+int count ( struct Node *p)
+{
+    int count = 0 ;
+    while (p)
+    {
+        count ++;
+        p=p->next;
+    }
+
+    return count;
+}
+
+int removeDuplicated( struct Node *p)
+{   
+    struct Node *q= p->next;
+
+    while (q!= NULL)
+    {
+        if(p->data != q->data)
+        {
+            p=q;
+            q=q->next;
+        }
+        else
+        {
+            p->next=q->next;
+            free(q);
+            q=p->next;
+        }
+    }
+}
+
+void display(struct Node*p)
+{
+    while (p!= NULL)
+    {
+        printf("%d ", p->data);
+        p=p->next;
+    }
+}
+
+int main ()
+{
+    int A[] = { 3, 5, 5, 7, 10 , 15};
+
+    create(A, 5);
+    removeDuplicated(first);
+
+    // index
+    delete(first , 3);
+    display(first);
+    return 0;
+}
