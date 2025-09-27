238cedf018bb2f35ee0f3f11d35f2aebffb4b839
Jasmeet Singh <jasmeetsonu990@gmail.com>
2025-09-27 16:24:11 +0530

    learning linked list

---
+++ by-abdul-bari/07-linked-list/10-is-sorted.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node * next;
+} * first = NULL;
+
+void create ( int A[], int n)
+{
+    int i;
+    struct Node *temp, *last;
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next= NULL;
+    last = first;
+
+    for (i=1; i<n; i++)
+    {
+        temp= (struct Node*)malloc(sizeof(struct Node));
+        temp->data = A[i];
+        temp->next = NULL;
+        last->next = temp;
+        last = temp;
+    }
+}
+
+int count ( struct Node *p)
+{
+    int count = 0 ;
+    while (p)
+    {
+        count ++;
+        p=p->next;
+    }
+
+    return count;
+}
+
+int isSorted( struct Node *p,int x)
+{   
+    int x = INT64_MIN;
+    while ( p!= NULL)
+    {
+        if(p->data<x)
+            return 0;
+        x = p->data;
+        p = p->next;
+    }
+    return 1;
+}
+
+void display(struct Node*p)
+{
+    while (p!= NULL)
+    {
+        printf("%d ", p->data);
+        p=p->next;
+    }
+}
+
+int main ()
+{
+    int A[] = { 3, 5, 7, 10 , 15};
+
+    create(A, 5);
+    display(first);
+
+    // index
+    delete(first , 3);
+    display(first);
+    return 0;
+}
