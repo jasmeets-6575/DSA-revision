ca9b171c438e9f66ae20015c82f3f5f7eb97d586
Jasmeet Singh <jasmeetsonu990@gmail.com>
2025-10-06 16:26:48 +0530

    learning linked list

---
+++ by-abdul-bari/07-linked-list/17-delete-circular-node.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+} * Head;
+
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    Head=(struct Node *)malloc(sizeof(struct Node));
+    Head->data= A[0];
+    Head->next= Head;
+    last= Head;
+
+    for (i=1; i<n; i++)
+    {
+        t=(struct Node *)malloc(sizeof(struct Node));
+        t->data=A[i];
+        t->next=last->next;
+        last->next=t;
+        last=t;
+    }
+}
+
+int Length( struct Node *p)
+{
+    int len=0;
+    do
+    {
+        len++;
+        p=p->next;
+    } while (p!=Head);
+    return len;
+}
+
+int Delete ( struct Node *p, int index) 
+{
+    struct Node *q=NULL;
+    int i, x;
+
+    if (index <0 || index > Length(p))
+        return -1;
+    
+    if (index == 1)
+    {
+        while (p->next != Head) p = p->next;
+        x= Head-> data;
+
+        if(Head == p)
+        {
+            free(Head);
+            Head= NULL;
+        }
+        else
+        {
+            p->next = Head->next;
+            free(Head);
+            Head = p->next;
+        }
+    }
+    else
+    {
+        for (i = 0; i < index - 2; i++) 
+            p=p->next;
+        q=p->next; 
+        p->next=q->next;
+        x=q->data;
+        free(q);
+    }
+    return x;
+}
+
+void Display( struct Node *h)
+{
+    do
+    {
+        printf("%d ",h->data);
+        h=h->next;
+    } while (h!=Head);
+    printf("\n\n");
+}
+
+void Rdisplay(struct Node *h)
+{
+    static int flag=0;
+
+    if(h != Head || flag==0)
+    {
+        flag=1;
+        printf("%d", h->data);
+        Rdisplay(h->next);
+    }
+    flag=0;
+}
+
+int main()
+{
+    int A[] = {2, 3, 4, 5, 6};
+    create(A, 5);
+
+    Delete(Head, 1);
+    Display(Head);
+
+    return 0;
+}
